d2499b7b3d1d5630a0a7d2087964020beb95cfc9
younguk2 <108333554+younguk2@users.noreply.github.com>
2022-07-06 11:16:53 +0900

    revise id/pwd servlet

--- back/servlet/IdFindServlet.java
+++ back/servlet/IdFindServlet.java
@@ -23,42 +23,47 @@ public class IdFindServlet extends HttpServlet {
 	/**
 	 * @see 아이디 찾기 버튼 눌렀을 때
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8");//ISO_88859_1
 		PrintWriter out=response.getWriter();//응답출력스트림 얻기
 		
 		String name=request.getParameter("name");
 		String id=request.getParameter("id");
+		
+		System.out.println("userName="+name+"userId="+id);
 		//DB와의 연결
 		Connection con=null;
 		//SQL송신
 		PreparedStatement pstmt=null;
 		//응답결과
 		ResultSet rs=null;
-		String result="{\"status\":0}";
-		
+		String result=null;
+		//"{\"status\":0}"
 		try {
 			con=MyConnection.getConnection();
-			String selectIdNPwdSQL="SELECT * FROM customer WHERE name=? AND id=?";
+			String selectIdNPwdSQL="SELECT * FROM customer WHERE userName=? AND userId=?";
 			pstmt=con.prepareStatement(selectIdNPwdSQL);
 			pstmt.setString(1, name);
 			pstmt.setString(2, id);
 			rs=pstmt.executeQuery();
-			if(rs.next()) {//입력한 정보와 일치하는 회원정보가 있으면 id값을 알려주자
-				String customerId = rs.getString("id");//DB에 저장되어 있는 회원 아이디를 찾고
-				out.print(customerId);//클라이언트에게 아이디 출력
+			System.out.println(rs);
+			if(!rs.next()) {//검색결과 나오지 않는다면?..
+				result="{\"status\":0}";
+			}else {//입력한 정보와 일치하는 회원정보가 있으면 id값을 알려주자
 				result="{\"status\":1}";//성공했다는 의미
-			}else {
-				
+				System.out.println(rs.getString("userId"));
+				String customerId = rs.getString("userId");//DB에 저장되어 있는 회원 아이디를 찾고
+				out.print(customerId);//클라이언트에게 아이디 출력
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
 			MyConnection.close(rs,pstmt,con);
 		}
-		
-		
 		out.print(result);
+		
 	}
-
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
--- back/servlet/PwdUpdateServlet.java
+++ back/servlet/PwdUpdateServlet.java
@@ -1,4 +1,4 @@
-package com.my.control;
+package com.my.projectServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,6 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,7 +19,7 @@ import com.my.sql.MyConnection;
 @WebServlet("/pwdupdate")
 public class PwdUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//요청전달데이터열기
 		response.setContentType("text/html;charset=UTF-8");
 		PrintWriter out=response.getWriter();
@@ -26,6 +27,7 @@ public class PwdUpdateServlet extends HttpServlet {
         //아이디 값과 사용자가 입력한 변경할 비밀번호 값을 받아오자 
 		String id=request.getParameter("id");
 		String pwd=request.getParameter("pwd");
+		System.out.println("userId="+id+" "+"userPwd="+pwd);
 		//DB와의 연결
 		Connection con=null;
 		//SQL송신
@@ -37,29 +39,22 @@ public class PwdUpdateServlet extends HttpServlet {
 		
         // 1) 비밀번호 변경 레이아웃에서 id값을 입력 받아서 
         //  비밀번호 변경할 때 아이디값을 가져와서 WHERE 절에 해당 아이디 값인 회원정보를 찾아 비밀번호를 UPDATE구문을 이용해 변경
-        // 2) 해당 페이지에선 상태는 이미 로그인된 상태이다 그러므로 로그인정보를 이용해서 비밀번호 변경해줄 회원정보를 특정하고
-        //  UPDATE문을 이용해서 비밀번호를 변경해준다.
 		try {
 			con=MyConnection.getConnection();
-			String selectIdNPwdSQL="UPDATE customer_info SET pw=? WHERE id=?";//해당 id인 회원정보의 pwd값을 update하자
+			String selectIdNPwdSQL="UPDATE customer SET userPwd=? WHERE userId=?";//해당 id인 회원정보의 pwd값을 update하자
 			pstmt=con.prepareStatement(selectIdNPwdSQL);
 			pstmt.setString(1, pwd);
 			pstmt.setString(2,id);
 			pstmt.executeUpdate();
+			result="{\"status\":1}";
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
 			MyConnection.close(rs,pstmt,con);
 		}
-		result="{\"status\":1}";
 		out.print(result);
 	}
-	/*
-				if(rs.next()) {
-                //비밀번호 변경이 성공적일 경우 status 1 값을 front로 전달 
-				result="{\"status\":1}";
-			}else {
-				
-			}
-	*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
