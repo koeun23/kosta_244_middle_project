e7d7d8f359f976633fa40939cad64c7ae19161b4
younguk2 <108333554+younguk2@users.noreply.github.com>
2022-07-07 10:04:24 +0900

    servlet change

--- back/servlet/PwdFindServlet.java
+++ back/servlet/PwdFindServlet.java
@@ -1,5 +1,3 @@
-package com.my.projectServlet;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -25,7 +23,7 @@ public class PwdFindServlet extends HttpServlet {
 	 * @see 비밀번호 찾기 버튼 눌렀을 떄
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");//ISO_88859_1
+		response.setContentType("application/json;charset=UTF-8");//ISO_88859_1
 		PrintWriter out=response.getWriter();//응답출력스트림 얻기
 		HttpSession session=request.getSession();
 		
@@ -33,13 +31,14 @@ public class PwdFindServlet extends HttpServlet {
 		String name=request.getParameter("name");
 		String id=request.getParameter("id");
 		String email=request.getParameter("email");
+		System.out.println("name="+name+" id="+id+" email="+email);
 		//DB와의 연결
 		Connection con=null;
 		//SQL송신
 		PreparedStatement pstmt=null;
 		//응답결과
 		ResultSet rs=null;
-		String result="{\"status\":0}";
+		String result=null;
 		
 		
 		try {
@@ -54,7 +53,7 @@ public class PwdFindServlet extends HttpServlet {
 				result="{\"status\":1}";
 				session.setAttribute("loginInfo", id);
 				//그리고 redirect를 이용해서 비밀번호 변경하는 페이지로 이동하자
-				response.sendRedirect("/front/pwdUpdate.html");
+				response.sendRedirect("http://localhost:8888/front/project_html/pwdUpdate.html");
 			}else {//입력한 정보와 일치하는 회원정보가 없다면
 				result="{\"status\":0}";
 			}
