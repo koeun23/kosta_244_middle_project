34a444e4515eaac4e06e5ab5f37fa42ac01bda07
younguk2 <108333554+younguk2@users.noreply.github.com>
2022-07-07 09:44:05 +0900

    serlvet change

--- back/servlet/IdFindServlet.java
+++ back/servlet/IdFindServlet.java
@@ -1,5 +1,22 @@
 
-@WebServlet("/Idfind")
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.my.sql.MyConnection;
+import com.my.projectdto.*;
+
+@WebServlet("/idfind")
 public class IdFindServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -7,13 +24,12 @@ public class IdFindServlet extends HttpServlet {
 	 * @see 아이디 찾기 버튼 눌렀을 때
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");//ISO_88859_1
+		response.setContentType("application/json;charset=UTF-8");//ISO_88859_1
 		PrintWriter out=response.getWriter();//응답출력스트림 얻기
 		
 		String name=request.getParameter("name");
-		String id=request.getParameter("id");
-		
-		System.out.println("userName="+name+"userId="+id);
+		String email=request.getParameter("email");
+		System.out.println("userName="+name+"userEmail="+email);
 		//DB와의 연결
 		Connection con=null;
 		//SQL송신
@@ -24,19 +40,20 @@ public class IdFindServlet extends HttpServlet {
 		//"{\"status\":0}"
 		try {
 			con=MyConnection.getConnection();
-			String selectIdNPwdSQL="SELECT * FROM customer WHERE userName=? AND userId=?";
+			String selectIdNPwdSQL="SELECT * FROM customer WHERE userName=? AND userEmail=?";
 			pstmt=con.prepareStatement(selectIdNPwdSQL);
 			pstmt.setString(1, name);
-			pstmt.setString(2, id);
+			pstmt.setString(2, email);
 			rs=pstmt.executeQuery();
 			System.out.println(rs);
-			if(!rs.next()) {//검색결과 나오지 않는다면?..
-				result="{\"status\":0}";
-			}else {//입력한 정보와 일치하는 회원정보가 있으면 id값을 알려주자
+			if(rs.next()) {//해당하는 아이가 있는지
 				result="{\"status\":1}";//성공했다는 의미
 				System.out.println(rs.getString("userId"));
 				String customerId = rs.getString("userId");//DB에 저장되어 있는 회원 아이디를 찾고
 				out.print(customerId);//클라이언트에게 아이디 출력
+				
+			}else {//입력한 정보와 일치하는 회원정보가 있으면 id값을 알려주자
+				result="{\"status\":0}";
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -44,7 +61,6 @@ public class IdFindServlet extends HttpServlet {
 			MyConnection.close(rs,pstmt,con);
 		}
 		out.print(result);
-		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
