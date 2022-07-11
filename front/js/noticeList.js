289f86fb0824e245644abb5531de64e9d6d00e7a
younguk2 <108333554+younguk2@users.noreply.github.com>
2022-07-11 12:15:44 +0900

    revise

--- front/js/noticeList.js
+++ front/js/noticeList.js
@@ -1,10 +1,10 @@
 $(function(){
-    //클릭한 게시글 번호를 a태그에 넣어서 
+	//클릭한 게시글 번호를 a태그에 넣어서 
 	//noticeView?boardNo=1
 	//이런식으로 ajax요청이 아닌 주소요청 방식(GET)으로 서버에 게시글번호를 주면
 	//서버에선 해당 글번호를 받아서 DB에 일치하는 게시글 정보를 찾고
 	//noticeView.html 페이지에 게시글 정보를 붙여ㅓ 사용자에게 보여준다
-    $(".linkNotice").click(function(){
+   	$(".linkNotice").click(function(){
 		//  div(글 정보포함 ) > div > a()
   		let $title=$(this);
   		let $divtitle=$title.parent();
@@ -16,6 +16,9 @@ $(function(){
   		console.log($boardNo);
    		let boardNo=$boardNo.html();
    		console.log(boardNo);
-   		$('.linkNotice').prop('href',"http://localhost:8888/back4/noticeview?boardNo="+boardNo);
+   		//$('.linkNotice').prop('href',"http://localhost:8888/back4/noticeview?boardNo="+boardNo);
+   		
+   		location.href="../project_html/noticeView.html?boardNo=" + boardNo;
+   		return false;
 	});
 });
