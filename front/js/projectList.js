ae4f71bbe9d1179dd57cdcc3cc68ec8cdc8027d7
wacheni <53849957+wacheni@users.noreply.github.com>
2022-07-12 12:00:46 +0900

    Update projectList.js

--- front/js/projectList.js
+++ front/js/projectList.js
@@ -1,51 +1,50 @@
-$(document).ready(function(){
-    //글쓰기 버튼 눌렀을 때
-    //projectWrite.html <a href=""~
+//on click 과 click의차이, on은 동적생성된 태에 이벤트가 바인딩된다.
+//그냥click은 미리 생성된 태그에만 이벤트가 바인딩된다.
+//$("#selector").on("click", "li", function(event){})
+//$("#selector").click(function(){});
+$(function(){
 
-    //user가 입력한 data를 sql구문에 추가, 그후에 sql구문에 있는 
-    //data를 받아서 각각 카테고리에 뿌려주기
-
-    //리스트 글중에 하나의 리스트에 있는 제목을 클릭했을 때
-    //모집글번호를 서버로 보내줄꺼고
-    //서버에서는 모집글번호를 받고 DB에서 보내진 모집글번호인 게시글 찾고
-    //서버에서 해당하는 글의 정보를 projectView.html 보내는 것 
-    //boardtitle이 클릭되었을 시에 발생할 이벤트들
-    $("div.boardtitle").click(function(){
-        let $board=$this.parent();
-        let $boardNo=$board.firstChild();
-
-        //모집글번호를 찾고
-        let p_no=$p_no.val();
-        //서버로 모집글번호를 전송
-        let url="back/projectview";
-        let data="p_no="+p_no;
-        //ajax는 servlet과 연결을 하는것에 필요한 파트임
-        $.ajax({
-            url:url,
-            method: 'post',
-            data: JSON,
-            //성공하면 success로 넘어가서 DB의 data를 html tag로 넘겨줌
-            success:function(){
-                $.each(query.content,function(data){
-                    html += "<tr>";
-                    html += "<th>"+this.p_no+"</th>";
-                    html += "<th>"+this.p_userno+"</th>";
-                    html += "<th>"+this.p_title+"</th>";
-                    html += "<th>"+this.p_content+"</th>";
-                    html += "<th>"+this.p_like+"</th>";
-                    html += "<th>"+this.p_views+"</th>";
-                    html += "<th>"+this.p_createday+"</th>";
-                    html += "</tr>";
-                });
-                $(".data").html(html);
-
-                //html 문자열로 받아서 뿌리기
-            },
-
-            error:function(jqXHE){
-                alert(jqXHR.error);
-            }
-        });
+    //post방식으로 임시목록 ajax를 호출한다.
+    HeyJs.fnAjaxPost(
+       {}
+       , "projectView"         //ajax 호출하는 URL
+       , fnAjaxListCallBack   
+    );
+  });
+ 
+ 
+ /****************************************
+ * @author me
+ * @desc ajax가 성공 후 호출하는 콜백 함수
+ ​*/       
+ let fnAjaxListCallBack = function(data){
+    
+    //ajax에서 받은 json형태의 결과값
+    let list = data.response;
+    
+    console.log('ajax 호출');
+    console.log(list);
+    
+    let appendHtml = "";
+ 
+    //동적으로 그리는 영역 초기화   
+    $('#setTempTbody').empty();
+    
+    list.forEach(function(v,i){ //포이치가 배열만큼 뺑뺑이돈다. 근데..v는 현재 뺑뺑이도는 현재 도는 위치의 값, i는 순번 index = i
+       
+       appendHtml +=   '<tr>';
+       appendHtml +=      '<td>'+i+'</td>'
+       appendHtml +=      '<td><a href="#">'+v.p_no+'</a></td>';
+       appendHtml +=      '<td>'+v.user_no+'</td>';
+       appendHtml += '<td>'+v.p_title+'</td>';
+       appendHtml += '<td>'+v.p_content+'</td>';
+       appendHtml += '<td>'+v.p_views+'</td>';
+       appendHtml +=      '<td>0</td>';
+       appendHtml +=      '<td>'+v.ptempCreateday+'</td>';
+       appendHtml +=   '</tr>'
+    
     });
-
-});
+    
+    $('#setTempTbody').append(appendHtml);
+ };
+ 
