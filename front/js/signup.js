2eba898e370c0b44418b7891b626462782e7e0dd
tyuiop3 <108501920+tyuiop3@users.noreply.github.com>
2022-07-13 11:23:27 +0900

    up

--- front/js/signup.js
+++ front/js/signup.js
@@ -1,116 +1,147 @@
-/*$(function(){
-	//가입하기 버튼을 눌렀을 때 발생할 이벤트
-	$('button.signupBtn').click(function(){
-		let url='http://';//아이디가 있는 back의 주소로
-		
-    });
-})*/
+$(function() {
+	//아이디입력객체찾기
+	let $inputId = $('.id');
+	/*var boolean = 0; // 실패중*/
+	//가입버튼객체찾기
+	let $btSubmit = $('button[type=submit]');
 
-$(function(){
-    //아이디입력객체찾기
-    let $inputId = $('input[name=id]');
-  
-    //가입버튼객체찾기
-    let $btSubmit = $('button[type=submit]');
-  
-    //--아이디중복확인버튼 클릭 START--
-    let $btIdchk = $('button.idchk');
-    $btIdchk.click(function(){
-      $.ajax({
-        url : 'http://localhost:8888/back/idchkservlet',
-        method : 'get',
-        data : {id: $inputId.val()},
-        success:function(jsonObj){
-          if(jsonObj.status == 1){ //사용가능한 아이디인 경우
-            $btSubmit.show();
-        }else{
-          alert(jsonObj.msg);
-        }  
-      },
-        error: function(jqSHR){
-          alert('오류' + jqSHR.status);
-        }
-  
-      });
-    
-      $btSubmit.show(); //기본이벤트처리가 데이터 전송 
-    })
-    //--아이디중복확인버튼 클릭 END--
-    
-    //--아이디입력란에 포커스 START--
-    
-    $inputId.focus(function(){
-      $btSubmit.hide();
-    });
-    //--아이디입력란에 포커스 END--
-    
-    //이메일입력객체찾기
-    let $inputemail = $('input[name=email]');
-  
-    //--이메일중복확인버튼 클릭 START--
-    let $btemailchk = $('button.emailchk');
-    $btemailchk.click(function(){
-      $.ajax({
-        url : 'http://localhost:8888/back/emailchkservlet',
-        method : 'get',
-        data : {id: $inputId.val()},
-        success:function(jsonObj){
-          if(jsonObj.status == 1){ //사용가능한 이메일인 경우
-            $btSubmit.show();
-        }else{
-          alert(jsonObj.msg);
-        }  
-      },
-        error: function(jqSHR){
-          alert('오류' + jqSHR.status);
-        }
-  
-      });
-    
-      $btSubmit.show(); //기본이벤트처리가 데이터 전송 
-    })
-    //--이메일중복확인버튼 클릭 END--
-    //--이메일입력란에 포커스 START--
-    
-    $inputemail.focus(function(){
-      $btSubmit.hide();
-    });
-    //--이메일입력란에 포커스 END--
-      //--폼 전송 START --
-    //가입버튼클릭이벤트발생->폼서브밋이벤트발생->기본처리(전송)
-    //폼객체찾기
-    let $form = $('div.signup>form');
-    $form.submit(function(){
-      alert('submit start');
-      //비밀번호 일치확인
-      let $pwd = $('div.signup input[name=pwd]');
-      let $pwdChk = $('#pwdChk');
-      if($pwd.val() != $pwdChk.val()){
-        alert('비밀번호가 일치하지 않습니다');
-        $pwd.focus();
-        return false; 
-      }  
-      let url = 'http://localhost:8888/back/signupservlet2';
-      let data = $(this).serialize(); //querystring만들어줌
-                  //ex)id=a&pwd=1&name=b&addr=c&buildingno=1
-      // alert(data);
-      $.ajax({
-        url: url,
-        method: 'post',
-        data: data,
-        // success:function(responseText){
-        //   let jsonObj = JSON.parse(responseText);
-        success: function(jsonObj){
-          alert(jsonObj.msg);
-        },
-        error : function(jqXHR){
-          alert('에러코드:' + jqXHR.status);
-        }
-      });
-      return false; 
-      
-    });
-    //--폼 전송 END--
-    
-    
-  });
+
+	//--아이디중복확인버튼 클릭 START--
+	let $btIdchk = $('button.idchk');
+
+	$btIdchk.click(function() {
+		$.ajax({
+			url: 'http://localhost:8888/recruit_project/idchk',
+			method: 'get',
+			data: { id: $inputId.val() },
+			success: function(jsonObj) {
+				if (jsonObj.status == 1) { //사용가능한 아이디인 경우
+					alert($inputId.val() + "는 사용가능합니다.");
+					$btSubmit.show();
+				} else {
+					alert(jsonObj.msg);
+					$('.id').focus();
+					$('.id').val("");
+					return false;
+				}
+			},
+			error: function(jqXHR) {
+				alert('오류' + jqXHR.status);
+			}
+
+		});
+
+		$btSubmit.show(); //기본이벤트처리가 데이터 전송 
+	})
+	//--아이디중복확인버튼 클릭 END--
+
+	//--아이디입력란에 포커스 START--
+
+	$inputId.focus(function() {
+		$btSubmit.hide();
+	});
+	//--아이디입력란에 포커스 END--
+
+	//이메일입력객체찾기
+	let $inputEmail = $('.email');
+
+	//--이메일중복확인버튼 클릭 START--
+	let $btemailchk = $('button.emailchk');
+	$btemailchk.click(function() {
+		$.ajax({
+			url: 'http://localhost:8888/recruit_project/emailchk',
+			method: 'get',
+			data: { email: $inputEmail.val() },
+			success: function(jsonObj) {
+				if (jsonObj.status == 1) { //사용가능한 이메일인 경우
+					alert($inputEmail.val() + "는(은) 사용가능한 이메일입니다.");
+					$btSubmit.show();
+				} else {
+					alert(jsonObj.msg);
+					$('.email').focus();
+					$('.email').val("");
+					return false;
+				}
+			},
+			error: function(jqXHR) {
+				alert('오류' + jqXHR.status);
+			}
+
+		});
+
+		$btSubmit.show(); //기본이벤트처리가 데이터 전송 
+	})
+	//--이메일중복확인버튼 클릭 END--
+	//--이메일입력란에 포커스 START--
+
+	$inputEmail.focus(function() {
+		$btSubmit.hide();
+	});
+	//--이메일입력란에 포커스 END--
+	//--폼 전송 START --
+	//가입버튼클릭이벤트발생->폼서브밋이벤트발생->기본처리(전송)
+	//폼객체찾기
+	let $form = $('form');
+	$form.submit(function() {
+		alert('submit start');
+		/*let $subBtn = $('button[type=submit]');
+		$subBtn.click(function(){
+		  alert('submit start');*/
+		//비밀번호 일치확인
+
+		let $pwd = $('input[name=pwd]');
+		let $pwdChk = $('input[name=pwdChk]');
+
+		if ($pwd.val() != $pwdChk.val()) {
+			alert('비밀번호가 일치하지 않습니다');
+			$pwd.focus();
+			return false;
+		}
+		else if ($pwd.val().length < 10) {
+			alert("비밀번호 길이가 너무 짧습니다");
+			$pwd.focus();
+			return false;
+		}else{
+			alert('옳바른 비번 길이');
+		}
+		//패스워드에 영문이랑 숫자가 혼합되었는지 확인하자
+		var passRule = /^(?=.*[A-Za-z0-9])(?=.*[!@#$%^*+=-])(?=..*[0-9]).{8,16}$/;
+		if (!passRule.test($pwd.val())) {
+			alert("비밀번호는 영문, 숫자, 특수문자 조합으로 10글자 이상이어야 합니다.");
+			return false;
+
+		}
+
+		let url = 'http://localhost:8888/recruit_project/signup';
+		//let data = $("#number").serialize(); //querystring만들어줌
+		let InputNameValue = $(".name").val();
+		let InputIdValue = $(".id").val();
+		let InputPwdValue = $pwd.val();
+		let InputEmailValue = $(".email").val();
+
+		let data = "name=" + InputNameValue + "&id=" + InputIdValue + "&pwd=" + InputPwdValue + "&email=" + InputEmailValue
+		
+		$.ajax({
+			url: url,
+			method: 'post',
+			data: data,
+			// success:function(responseText){
+			//  let jsonObj = JSON.parse(responseText);
+			success: function(jsonObj) {
+				alert(jsonObj.msg);
+				if (jsonObj.status == 1)
+					location.href = "mainPage.html";
+			},
+			error: function(jqXHR) {
+				alert('에러코드:' + jqXHR.status);
+			}
+
+		});
+		return false;
+
+
+	});
+
+	//--폼 전송 END--
+
+});
