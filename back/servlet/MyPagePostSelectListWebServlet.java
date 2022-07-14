2b7fd896501454a3d4f20711315aa5917700c1e2
hyepark0826 <107901144+hyepark0826@users.noreply.github.com>
2022-07-14 09:30:17 +0900

    MyPaePostSelectListWebServlet.java

---
+++ back/servlet/MyPagePostSelectListWebServlet.java
@@ -0,0 +1,73 @@
+package hye.myPage.servlet;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import hye.myPage.dto.MyPagePostDTO;
+import hye.myPage.dto.ResultDTO;
+import hye.myPage.repository.MyPagePostRepository;
+/**
+ * Servlet implementation class MyPageWebServlet
+ */
+
+//내가 정의한 URL을 입력하면
+//request.getRequestDispatcher 여기에 정의한 html로 이동시켜준다.
+@WebServlet("/myPagePostSelectList")
+public class MyPagePostSelectListWebServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+       
+    public MyPagePostSelectListWebServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+		
+	}
+
+
+	/**
+	 * 나의 작성글 목록을 조회한다.
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("ajax를 doPost로 호출한다. ");
+
+		//ResultDTO란???? ajax 응답결과를 공통 보여주게 한다.
+		ResultDTO<List<MyPagePostDTO>> resp = new ResultDTO<>();
+		
+		MyPagePostRepository repository =new MyPagePostRepository();
+
+		try {
+			//임시저장글 목록을 조회한다.
+			List<MyPagePostDTO> list = repository.postProjectList();
+			// 에러가 발생시 처리한다. 응답 response에 오류를 알린다.
+			resp.setResponse(list);
+		} catch (Exception e) {
+			resp.setMessage(e.toString());
+			resp.setCheck(false);
+		}
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        //Gson 라이브러리로 json을 리턴한다.
+        new Gson().toJson(resp, response.getWriter());
+        
+	}
+
+}
