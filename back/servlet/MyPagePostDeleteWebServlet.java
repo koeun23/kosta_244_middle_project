f77881516080af9ca50cffef04adf7b13492966a
hyepark0826 <107901144+hyepark0826@users.noreply.github.com>
2022-07-18 18:39:05 +0900

    MyPagePostDeleteWebServlet.java

---
+++ back/servlet/MyPagePostDeleteWebServlet.java
@@ -0,0 +1,87 @@
+package hye.myPage.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import hye.myPage.dto.MyPagePostDTO;
+import hye.myPage.dto.MyPageTempDTO;
+import hye.myPage.dto.ResultDTO;
+import hye.myPage.repository.MyPagePostRepository;
+
+/**
+ * Servlet implementation class MyPageWebServlet
+ */
+@WebServlet("/myPagePostDelete")
+public class MyPagePostDeleteWebServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public MyPagePostDeleteWebServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}		
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	/*
+	 * 임시글 삭제 ajax 호출
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		System.out.println("myPagePostDelete ajax를 doPost로 호출한다. ");
+
+		// ResultDTO란???? ajax 응답결과를 공통 보여주게 한다.
+		ResultDTO<Integer> resp = new ResultDTO<>();
+
+		int result = 0;
+
+		// 글 번호
+		String pNo = request.getParameter("pNo");
+
+		System.out.println("pNo : " + pNo);
+
+		MyPagePostRepository repository = new MyPagePostRepository();
+
+		MyPagePostDTO dto = new MyPagePostDTO();
+
+		dto.setpNo(Integer.parseInt(pNo));
+
+		try {
+			// repository에서 delete sql문이 성공하면 1을 반환한다.
+			result = repository.myPagePostDelete(dto);
+
+			// 에러가 발생시 처리한다. 응답 response에 오류를 알린다.
+			resp.setResponse(result);
+		} catch (Exception e) {
+			resp.setMessage(e.toString());
+			resp.setCheck(false);
+		}
+
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		// Gson 라이브러리로 json을 리턴한다.
+		new Gson().toJson(resp, response.getWriter());
+
+	}
+
+}
+
